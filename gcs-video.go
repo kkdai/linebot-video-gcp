80bbf9d9a476c3edcdb517c8f5258e7c37a5fd48
Evan Lin <evan.if.lin@linecorp.com>
2022-03-16 23:44:17 +0800

    add gcs

--- gcs-video.go
+++ gcs-video.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"time"
+)
+
+const (
+	projectID  = "your-project-id"  // FILL IN WITH YOURS
+	bucketName = "your-bucket-name" // FILL IN WITH YOURS
+)
+
+type ClientUploader struct {
+	cl         *storage.Client
+	projectID  string
+	bucketName string
+	uploadPath string
+}
+
+// UploadFile uploads an object
+func (c *ClientUploader) UploadFile(file multipart.File, object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	// Upload an object with storage.Writer.
+	wc := c.cl.Bucket(c.bucketName).Object(c.uploadPath + object).NewWriter(ctx)
+	if _, err := io.Copy(wc, file); err != nil {
+		return fmt.Errorf("io.Copy: %v", err)
+	}
+	if err := wc.Close(); err != nil {
+		return fmt.Errorf("Writer.Close: %v", err)
+	}
+
+	return nil
+}
