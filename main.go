39a89504941416927b42a95113c179bc322cb19d
Evan Lin <evan.if.lin@linecorp.com>
2022-03-25 12:51:29 +0800

    rollback

--- main.go
+++ main.go
@@ -79,64 +79,14 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							},
 							AspectRatio: linebot.FlexVideoAspectRatioType20to13,
 						},
-
-						Body: &linebot.BoxComponent{
-							Type:   linebot.FlexComponentTypeBox,
-							Layout: linebot.FlexBoxLayoutTypeVertical,
-							Contents: []linebot.FlexComponent{
-								&linebot.TextComponent{
-									Type:   linebot.FlexComponentTypeText,
-									Text:   "Title",
-									Weight: linebot.FlexTextWeightTypeBold,
-									Size:   linebot.FlexTextSizeTypeXl,
-									Align:  linebot.FlexComponentAlignTypeCenter,
-								},
-							},
-						}}).MarshalJSON()
+					}).MarshalJSON()
 				if err != nil {
 					log.Print(err)
 				}
-
-				vdoContainer, _ := linebot.UnmarshalFlexMessageJSON([]byte(`{
-					"type": "bubble",
-					"size": "mega",
-					"hero": {
-					  "type": "video",
-					  "url": "https://example.com/video.mp4",
-					  "previewUrl": "https://example.com/video_preview.png",
-					  "altContent": {
-						"type": "image",
-						"size": "full",
-						"aspectRatio": "20:13",
-						"aspectMode": "cover",
-						"url": "https://example.com/image.png"
-					  },
-					  "action": {
-						"type": "uri",
-						"label": "More information",
-						"uri": "http://linecorp.com/"
-					  },
-					  "aspectRatio": "20:13"
-					},
-					"body": {
-					  "type": "box",
-					  "layout": "vertical",
-					  "contents": [
-						{
-						  "type": "text",
-						  "text": "Brown Cafe",
-						  "weight": "bold",
-						  "size": "xl"
-						}
-					  ]
-					}
-				  }`))
-
-				json2, _ := linebot.NewFlexMessage("video", vdoContainer).MarshalJSON()
 				log.Print(">>>>\n", string(json))
 				ret := string(json)
 
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(ret), linebot.NewTextMessage(string(json2))).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(ret)).Do(); err != nil {
 					log.Print(err)
 				}
 			// Handle only on Sticker message
@@ -249,44 +199,28 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 					vdourl := uploader.GetPulicAddress()
 
-					vdoContainer, _ := linebot.UnmarshalFlexMessageJSON([]byte(`{
-						"type": "bubble",
-						"size": "mega",
-						"hero": {
-						  "type": "video",
-						  "url": "` + vdourl + `",
-						  "previewUrl": "https://example.com/video_preview.png",
-						  "altContent": {
-							"type": "image",
-							"size": "full",
-							"aspectRatio": "20:13",
-							"aspectMode": "cover",
-							"url": "https://example.com/image.png"
-						  },
-						  "action": {
-							"type": "uri",
-							"label": "More information",
-							"uri": "http://linecorp.com/"
-						  },
-						  "aspectRatio": "20:13"
-						},
-						"body": {
-						  "type": "box",
-						  "layout": "vertical",
-						  "contents": [
-							{
-							  "type": "text",
-							  "text": "Brown Cafe",
-							  "weight": "bold",
-							  "size": "xl"
-							}
-						  ]
-						}
-					  }`))
-
+					vdo := &linebot.BubbleContainer{
+						Type: linebot.FlexContainerTypeBubble,
+						Hero: &linebot.VideoComponent{
+							Type:       linebot.FlexComponentTypeVideo,
+							URL:        vdourl,
+							PreviewURL: "https://example.com/video_preview.png",
+							AltContent: &linebot.ImageComponent{
+								Type:        linebot.FlexComponentTypeImage,
+								URL:         "https://example.com/image.png",
+								Size:        linebot.FlexImageSizeTypeFull,
+								AspectRatio: linebot.FlexImageAspectRatioType20to13,
+								AspectMode:  linebot.FlexImageAspectModeTypeCover,
+							},
+							Action: &linebot.URIAction{
+								Label: "More information",
+								URI:   "http://linecorp.com/",
+							},
+							AspectRatio: linebot.FlexVideoAspectRatioType20to13,
+						}}
 					if _, err = bot.ReplyMessage(event.ReplyToken,
 						linebot.NewTextMessage(ret),
-						linebot.NewFlexMessage("video", vdoContainer)).Do(); err != nil {
+						linebot.NewFlexMessage("video", vdo)).Do(); err != nil {
 						log.Print(err)
 					}
 
