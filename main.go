c311b742308a979b3696b2ea992a7608fa4b49c4
Evan Lin <evan.if.lin@linecorp.com>
2022-04-01 16:27:24 +0800

    add detect

--- main.go
+++ main.go
@@ -188,6 +188,7 @@ func uploadAndDectect(uid string, msg *linebot.VideoMessage, bot *linebot.Client
 			uploadPath: "test-files/",
 		}
 
+		// Upload video to Google Cloud Storage
 		err = uploader.UploadVideo(content.Content)
 		if err != nil {
 			ret = "uploader.UploadFile: " + err.Error()
@@ -196,7 +197,14 @@ func uploadAndDectect(uid string, msg *linebot.VideoMessage, bot *linebot.Client
 		}
 
 		vdourl := uploader.GetPulicAddress()
-		flx := newVideoFlexMsg(vdourl, "video link:"+vdourl+" ret:"+ret)
+
+		var ret string
+		// Detect string from video
+		if err, ret = uploader.SpeachToText(); err != nil {
+			log.Print(err)
+		}
+
+		flx := newVideoFlexMsg(vdourl, ret)
 
 		if _, err = bot.PushMessage(uid, linebot.NewFlexMessage("flex", flx)).Do(); err != nil {
 			log.Print(err)
